97fd94970fdeadf18f31e9e6156fc6ed07fc504e
MJJ <mjj2000@gmail.com>
2013-04-23 15:11:42 +0800

    - use grunt-regarde instead of grunt-contrib-watch  - fix grunt-contrib-livereload to support random port

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,44 +2,56 @@ module.exports = function(grunt) {
   'use strict';
   var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet
     , folderMount = function (connect, dir) {
-        return connect.static(require('path').resolve(dir));
+        return connect.static(require('path').resolve(dir), { maxAge: 0 });
+      }
+    , config = {
+        app: './app',
+        css: '<%= config.app %>/css',
+        js: '<%= config.app %>/js'
       };
-
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      scripts: {
-        files: ['app/*'],
-        tasks: ['compass', 'livereload'],
-        options: {}
+    config: config,
+    regarde: {
+      compass: {
+        files: ['<%= config.css %>/{,*/}*.{scss,sass}'],
+        tasks: ['compass']
+      },
+      livereload: {
+        files: [
+          '<%= config.app %>/*.html',
+          '<%= config.app %>/images/{,*/}*.*',
+          '<%= config.css %>/{,*/}*.css',
+          '<%= config.css %>/images/{,*/}*.*',
+          '<%= config.js %>/scripts/{,*/}*.js'
+        ],
+        tasks: ['livereload']
       }
     },
     compass: {                  // Task
       dist: {                   // Target
         options: {              // Target options
-          sassDir: 'app',
-          cssDir: 'app',
+          sassDir: '<%= config.css %>',
+          cssDir: '<%= config.css %>',
           environment: 'production'
         }
       }
     },
     livereload: {
-        port: '8087' // Default livereload listening port.
+        port: 0 // Default livereload listening port.
       , hostname: '*'
     },
     connect: {
-      'server.livereload': {
+      server: {
         options: {
-            hostname: '*'
-          , port: 8086
+            hostname: '0.0.0.0'
+          , port: 0
           , base: 'app'
-          , keepalive: true
-          , middleware: function(connect, options) {
+          , middleware: function (connect, options) {
               return [
-                connect.static(options.base),
                 lrSnippet,
-                connect.directory(options.base),
+                folderMount(connect, options.base),
+                connect.directory(options.base)
               ];
             }
         }
@@ -47,16 +59,15 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-regarde');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-livereload');
-
   // Default task(s).
   grunt.registerTask('default', [
-    // 'livereload-start',
-    'connect',
-    // 'watch'
+    'livereload-start',
+    'connect:server',
+    'regarde'
   ]);
 
 };
